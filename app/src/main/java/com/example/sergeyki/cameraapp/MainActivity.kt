ef7fdeb5c2b085824defb7b59245cf0ce9e87bbb
Sergey Kim <sergey.kim@valuecode.com>
2018-11-03 20:35:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sergeyki/cameraapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.sergeyki.cameraapp
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val cameraRequestCode = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        cameraButton.setOnClickListener {
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (callCameraIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(callCameraIntent, cameraRequestCode)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            cameraRequestCode -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    photoImageView.setImageBitmap(data.extras?.get("data") as Bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
